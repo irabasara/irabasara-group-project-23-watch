f188f18ac70dd082ab881ebbc72a7246933f1956
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-04-21 18:48:03 +0200

    create swiper

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 // SWIPER
 const swiper = new Swiper(".mySwiper", {
     sledesPerView: 1,
-    spaceBetween: 5,
+    spaceBetween: 10,
     loop: true,
     keyboard: {
         enabled: true,
     },
     navigation: {
-        nextEl: ".button-next",
-        prevEl: ".button-prev",
+        nextEl: '.button-next',
+        prevEl: '.button-prev'
     },
-
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return '0' + '<span class="' + currentClass + '"></span>' + '<span class="pagination-line"></span>' + '0' + '<span class="' + totalClass + '"></span>';
+        }
+    }
 })
